375c25ddcd66e53fbba9c4d0ad2b9fe69d774cb1
hao <im_leihao@163.com>
2024-05-19 10:52:10 +0800

    采用整个文件进行转码

--- inc/convert.h
+++ inc/convert.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <errno.h>
 
 int convert(const char *path);
 
--- src/convert.c
+++ src/convert.c
@@ -36,13 +36,29 @@ static int gbk_to_utf8(char *inbuf, size_t *inlen, char *outbuf, size_t *outlen)
      *
      * */
     size_t ret = iconv(icp, &tmpin, inlen, &tmpout, outlen);
-    if (ret == -1) {
-        perror("iconv fail\n");
-        return -1;
+    if (ret == (size_t)-1) {
+        if (errno == E2BIG) {
+            // 输出缓冲区空间不足
+            fprintf(stderr, "Output buffer too small\n");
+            iconv_close(icp);
+            return -1;
+        } else if (errno == EILSEQ || errno == EINVAL) {
+            // 非法的多字节序列或不完整的多字节序列
+            fprintf(stderr, "Invalid or incomplete multibyte sequence\n");
+            iconv_close(icp);
+            return -1;
+        } else {
+            // 其他错误
+            perror("iconv fail");
+            iconv_close(icp);
+            return -1;
+        }
     }
 
     /* 关闭句柄 */
     iconv_close(icp);
+    // 更新输出长度
+    *outlen -= outsize;
 
     return 0;
 }
@@ -52,8 +68,9 @@ int convert(const char *path)
     // 释放内存等其他操作
     FILE *fp = NULL;
     FILE *target = NULL;
-
-    char origin_buff[2049], des[2049];
+    uint32_t file_size = 0;
+    char *origin = NULL, *des = NULL;
+    size_t ret = 0, srclen, outlen;
 
     if (!is_file_gbk(path)) {
         printf("%s 不是一个gbk编码，暂不支持转化\n", path);
@@ -67,76 +84,110 @@ int convert(const char *path)
         return -1;
     }
 
-    size_t ret = 0, srclen, outlen = 2048;
-
     strcpy(back_file, path);
     strcat(back_file, ".back");
 
     target = fopen(back_file, "w+");
     if (NULL == target) {
         printf("临时文件创建失败\n");
+        free(back_file);
         return -1;
     }
 
     fp = fopen(path, "r");
     if (NULL == fp) {
         printf("%s 打开失败\n", path);
-        goto FAIL_FREE_MEM;
+        fclose(target);
+        free(back_file);
+        return -1;
     }
 
-    // 逐行读取文件内容
-    while (fgets(origin_buff, 2048, fp) != NULL) {
 
-        /* 得到一行内容的长度 */
-        srclen = strlen(origin_buff);
+    fseek(fp, 0, SEEK_END);
+    file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
 
-        //进行字符编码转换，GBK ---> UTF-8
-        ret = gbk_to_utf8(origin_buff, &srclen, des, &outlen);
-        //ret = utf8_to_gbk(origin_buff, &srclen, convert, &outlen);
-        if (-1 == ret) {
-            if (remove(back_file) != 0) {
-                goto FAIL_FREE_MEM;
-            }
-            goto FAIL_FREE_MEM;
-        }
-
-        //将转换后的文件写到新的文件中
-        fwrite(des, 1, strlen(des), target);
-        fflush(target);
-
-        //清空转换结果缓冲区
-        memset(des, 0, sizeof(des));
-        outlen = sizeof(des);
+    origin = (char *)calloc(file_size + 4, 1);
+    if (NULL == origin) {
+        fclose(fp);
+        fclose(target);
+        free(back_file);
+        return -1;
     }
+
+    des = (char *)calloc((file_size*4) + 4, 1);
+    if (NULL == des) {
+        fclose(fp);
+        fclose(target);
+        free(back_file);
+        free(origin);
+        return -1;
+    }
+
+    // 读取源文件内容
+    if (fread(origin, 1, file_size, fp) != file_size) {
+        perror("文件读取失败");
+        fclose(fp);
+        fclose(target);
+        free(back_file);
+        free(origin);
+        free(des);
+        return -1;
+    }
+
+    //进行字符编码转换，GBK ---> UTF-8
+    srclen = file_size;
+    outlen = file_size * 4;
+    ret = gbk_to_utf8(origin, &srclen, des, &outlen);
+    if (-1 == ret) {
+        fclose(fp);
+        fclose(target);
+        remove(back_file);
+        free(back_file);
+        free(des);
+        free(origin);
+        return -1;
+    }
+
+    // 将转换后的内容写入目标文件
+    if (fwrite(des, 1, strlen(des), target) != strlen(des)) {
+        perror("文件写入失败");
+        fclose(fp);
+        fclose(target);
+        remove(back_file);
+        free(back_file);
+        free(origin);
+        free(des);
+        return -1;
+    }
+    fflush(target);
+
+    //关闭文件句柄
+    fclose(fp);
+    fclose(target);
 
     // 删除源文件
     if (remove(path) != 0) {
         printf("删除源文件%s失败", path);
-        goto FAIL_FREE_MEM;
+        free(back_file);
+        free(origin);
+        free(des);
+        return -1;
     }
 
     // 重命名目标文件为源文件名
     if (rename(back_file, path) != 0) {
         perror("重命名文件失败");
-        goto FAIL_FREE_MEM;
+        free(back_file);
+        free(origin);
+        free(des);
+        return -1;
     }
 
-    //关闭文件句柄
-    fclose(fp);
-    fclose(target);
-
     /* 释放掉备份文件路径 */
     free(back_file);
+    free(origin);
+    free(des);
 
     return 0;
-
-/*错误，释放资源*/
-FAIL_FREE_MEM:
-    //关闭文件句柄
-    fclose(fp);
-    fclose(target);
-
-    /* 释放掉备份文件路径 */
-    free(back_file);
-    return -1;
 }
